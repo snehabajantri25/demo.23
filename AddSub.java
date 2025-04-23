f9000fe60bd113f2d3894369a1ad760b2adf2c18
ruhii01 <ruhaniroshan01@gmail.com>
2025-04-23 14:20:45 +0530

    save

---
+++ AddSub.java
@@ -0,0 +1,12 @@
+public class AddSub {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20; 
+
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+
+        System.out.println("Addition Result: " + sum);
+        System.out.println("Subtraction Result: " + difference);
+    }
+}
